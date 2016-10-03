7a3000eec4c04333813450b9657cf76bc801340d
ABU TALHA <talhaqc@gmail.com>
2016-10-04 04:32:13 +0600

    activate page edited

--- resources/views/emails/activation.blade.php
+++ resources/views/emails/activation.blade.php
@@ -1,76 +1,145 @@
-<!DOCTYPE html>
-<html lang="en-US">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" style="font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
 <head>
-    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> Track Platoon Sign Up</title>
+    <style type="text/css">
+        img {
+            max-width: 100%;
+        }
+        body {
+            -webkit-font-smoothing: antialiased;
+            -webkit-text-size-adjust: none;
+            width: 100% !important;
+            height: 100%;
+            line-height: 1.6em;
+        }
+        body {
+            background-color: #f6f6f6;
+        }
+        @media only screen and (max-width: 640px) {
+            body {
+                padding: 0 !important;
+            }
+            h1 {
+                font-weight: 800 !important;
+                margin: 20px 0 5px !important;
+            }
+            h2 {
+                font-weight: 800 !important;
+                margin: 20px 0 5px !important;
+            }
+            h3 {
+                font-weight: 800 !important;
+                margin: 20px 0 5px !important;
+            }
+            h4 {
+                font-weight: 800 !important;
+                margin: 20px 0 5px !important;
+            }
+            h1 {
+                font-size: 22px !important;
+            }
+            h2 {
+                font-size: 18px !important;
+            }
+            h3 {
+                font-size: 16px !important;
+            }
+            .container {
+                padding: 0 !important;
+                width: 100% !important;
+            }
+            .content {
+                padding: 0 !important;
+            }
+            .content-wrap {
+                padding: 10px !important;
+            }
+            .invoice {
+                width: 100% !important;
+            }
+        }
+    </style>
 </head>
-<body>
-<h2>Verify Your Confirmation Code</h2>
-<table class="container-middle" align="center" border="0" cellpadding="0" cellspacing="0" width="560" bgcolor="F1F2F7">
-    <tbody><tr>
-        <td>
-            <table class="mainContent" align="center" border="0" cellpadding="0" cellspacing="0" width="528">
-                <tbody><tr><td height="20"></td></tr>
-                <tr>
-                    <td>
+<body itemscope itemtype="#" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; -webkit-font-smoothing: antialiased; -webkit-text-size-adjust: none; width: 100% !important; height: 100%; line-height: 1.6em; background-color: #f6f6f6; margin: 0;" bgcolor="#f6f6f6">
+<table class="body-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background-color: #f6f6f6; margin: 0;" bgcolor="#f6f6f6">
+    <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+        <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;" valign="top"></td>
+        <td class="container" width="600" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto;" valign="top">
+            <div class="content" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
+                <center>
+                    <table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background-color: #fff; margin: 0; border: 1px solid #e9e9e9;" bgcolor="#fff">
+                        <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                            <td class="alert alert-warning" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 16px; vertical-align: top; color: #fff; font-weight: 500; text-align: center; border-radius: 3px 3px 0 0; background-color: #2f353f; margin: 0; padding: 20px;" align="center" bgcolor="#2f353f" valign="top">
+                                <b>Track Platoon</b>
 
-                        <table class="section-item" align="left" border="0" cellpadding="0" cellspacing="0" width="360">
-                            <tbody><tr>
-                                <td style="color: #484848; font-size: 16px; font-weight: normal; font-family: Helvetica, Arial, sans-serif;">
-
-                                    Sign Up Confirmation
-
-                                </td>
-                            </tr>
-                            <tr><td height="15"></td></tr>
-                            <tr>
-                                <td style="color: #a4a4a4; line-height: 25px; font-size: 12px; font-weight: normal; font-family: Helvetica, Arial, sans-serif;">
-
-                                    <div>
-                                        Dear user, thanks for creating an account.
-                                        Please use this code to Confirm your account.
-                                        .<br/>
-
-                                    </div>
-
-                                </td>
-                            </tr>
-                            <tr><td height="15"></td></tr>
-                            <tr>
-                                <td>
-                                    <h1 style="background-color: #7087A3; font-size: 12px; padding: 10px 15px; color: #fff; text-decoration: none"> {{ $confirm_code }} </h1>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
+                            </td>
+                        </tr>
+                        <tr style=" font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                            <td class="content-wrap" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 20px;" valign="top">
+                                <center>
+                                    <table width="100%" cellpadding="0" cellspacing="0" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                        <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                                            <td class="content-block" style="text-align:center;  font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+                                                <br>
+                                                <h2>Sign Up Confirmation</h2>
 
 
-                        <table align="left" border="0" cellpadding="0" cellspacing="0">
-                            <tbody><tr><td height="30" width="30"></td></tr>
-                            </tbody>
-                        </table>
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td style="color: #a4a4a4; text-align:center; line-height: 25px; font-size: 14px; font-weight: normal; font-family: Helvetica, Arial, sans-serif;">
+
+                                                <p>
+                                                    Dear user, thanks for creating an account.
+                                                    Please use this code to Confirm your account.
+                                                    .<br/>
+
+                                                </p>
+
+                                            </td>
+                                        </tr>
 
 
-                        {{--<table class="section-item" align="left" border="0" cellpadding="0" cellspacing="0">--}}
-                            {{--<tbody><tr><td height="6"></td></tr>--}}
-                            {{--<tr>--}}
-                                {{--<td><a href="" style="width: 128px; display: block;">{{ Html::image('img/logo_dark.png', 'alt', array( 'width' => 35, 'height' => 35 )) }}</a></td>--}}
-                            {{--</tr>--}}
-                            {{--<tr><td height="10"></td></tr>--}}
-                            {{--</tbody>--}}
-                        {{--</table>--}}
+                                        <tr>
+                                            <td>
+                                                <h1 style="background-color: #7087A3; text-align:center; font-size: 17px; padding: 10px 1px; color: #fff; text-decoration: none"> {{ $confirm_code }} </h1>
+                                                <br>
+                                            </td>
+                                        </tr>
 
 
-                    </td>
-                </tr>
 
-                <tr><td height="20"></td></tr>
 
-                </tbody></table>
+
+
+
+
+
+
+
+
+                                    </table>
+                                </center>
+                            </td>
+                        </tr>
+                    </table>
+                </center>
+                <div class="footer" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; clear: both; color: #999; margin: 0; padding: 20px;">
+                    <table width="100%" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                        <tr style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+                            <td class="aligncenter content-block" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; vertical-align: top; color: #999; text-align: center; margin: 0; padding: 0 0 20px;" align="center" valign="top">
+                                <a href="http://trackplatoonapp.com/" target="_blank" style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 12px; color: #999; text-decoration: underline; margin: 0;">Track Platoon</a> visit our web page</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </td>
+        <td style="font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;" valign="top"></td>
     </tr>
-
-
-    </tbody></table>
-
+</table>
 </body>
 </html>
