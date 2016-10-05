fe495ba0e7539ca04c8fece7f97b17df1f118ee1
ABU TALHA <talhaqc@gmail.com>
2016-10-05 14:02:44 +0600

    sub topic seeded

--- database/seeds/PostSubTypeTableSeeder.php
+++ database/seeds/PostSubTypeTableSeeder.php
@@ -12,20 +12,157 @@ class PostSubTypeTableSeeder extends Seeder
      */
     public function run()
     {
-        PostSubType::create([ 'post_type_id' => 1,'name' => 'X']);
-        PostSubType::create([ 'post_type_id' => 1,'name' => 'Y']);
-        PostSubType::create([ 'post_type_id' => 1,'name' => 'Z']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Public Nonsense']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Illegal Activity']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Religious Violence']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Illegal Subscription']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Bad People Nuisance']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Implantation Rumor']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Negligence On Public Duty']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Social Decadence']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Public Safety']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Child Labor']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Persecution Poor People']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'For People']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Petty Corruption']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Grand Corruption']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Systemic Corruption']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Political Corruption']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Police Corruption']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Forensic Corruption']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Judicial Corruption']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Educational Corruption']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Corruption on Public Duty']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Murder']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Robbery']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Stealing']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Sexual Harassment']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Illegal Drug Trade']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Illegal Weapon']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Juvenile Crime']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Cyber Crime']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Driving and Traffic Crime']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Mugging']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Fraud']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Climate Change']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Environment Pollution']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Ocean Pollution']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'River Pollution']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Burning Forest']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Clean Energy']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Broken Road']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Electricity Problem']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Dustbin Problem']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Energy Problem']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Labor Rights']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Human Rights']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Wild Animal Rights']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Child Rights']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Poor People Rights']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Adulterant Product']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Adulterant Food']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Adulterant Medicine']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Medical Advice']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Law Advice']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Police Advice']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Safety Advice']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Social Advice']);
+        PostSubType::create([ 'post_type_id' => 1,'name' => 'Traffic Advice']);
 
-        PostSubType::create([ 'post_type_id' => 2,'name' => 'X']);
-        PostSubType::create([ 'post_type_id' => 2,'name' => 'Y']);
-        PostSubType::create([ 'post_type_id' => 2,'name' => 'Z']);
 
-        PostSubType::create([ 'post_type_id' => 3,'name' => 'X']);
-        PostSubType::create([ 'post_type_id' => 3,'name' => 'Y']);
-        PostSubType::create([ 'post_type_id' => 3,'name' => 'Z']);
 
-        PostSubType::create([ 'post_type_id' => 4,'name' => 'X']);
-        PostSubType::create([ 'post_type_id' => 4,'name' => 'Y']);
-        PostSubType::create([ 'post_type_id' => 4,'name' => 'Z']);
+
+
+        PostSubType::create([ 'post_type_id' => 2,'name' => ' Public Nonsense']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Public Nonsense']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Illegal Activity']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Religious Violence']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Illegal Subscription']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Bad People Nuisance']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Implantation Rumor']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Negligence On Public Duty']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Social Decadence.']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Public Safety']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Child Labor']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Persecution Poor People.']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Petty Corruption']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Grand Corruption']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Systemic Corruption']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Political Corruption']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Police Corruption']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Forensic Corruption']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Judicial Corruption']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Educational Corruption']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Corruption on Public Duty']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Robbery']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Stealing']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Sexual Harassment']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Illegal Drug Trade']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Juvenile Crime']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Cyber Crime ']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Driving and Traffic Crime']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Mugging']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Fraud']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Others']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Climate Change']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Environment Pollution']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Ocean Pollution']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'River Pollution']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Burning Forest']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Clean Energy']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Broken Road']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Electricity Problem']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Dustbin Problem']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Energy Problem']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Labor Rights']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Human Rights']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Wild Animal Rights']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Child Rights']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Poor People Rights.']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Adulterant Product']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Adulterant Food']);
+        PostSubType::create([ 'post_type_id' => 2,'name' => 'Adulterant Medicine']);
+
+
+
+
+
+
+        PostSubType::create([ 'post_type_id' => 3,'name' => 'Blood']);
+        PostSubType::create([ 'post_type_id' => 3,'name' => 'Kidney']);
+        PostSubType::create([ 'post_type_id' => 3,'name' => 'Medical']);
+        PostSubType::create([ 'post_type_id' => 3,'name' => 'Support']);
+        PostSubType::create([ 'post_type_id' => 3,'name' => 'Personal']);
+        PostSubType::create([ 'post_type_id' => 3,'name' => 'Voluntary']);
+        PostSubType::create([ 'post_type_id' => 3,'name' => 'Humanity Purpose']);
+
+
+
+
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'City Development']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Village Development']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Poor Child Development']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Poor People Development']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Educational Development']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Climate Change']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Saving Wildlife']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Environmental Development']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Clean Energy']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Blood Donate']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Donate free medicine']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Donate Cloth']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Donate Food']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Social Awareness']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Public Safety']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Helping Poor People.']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Natural disaster Help']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Relief Help']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Free Education']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Free Medical Service']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Free Home Nursing Training']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Free Training']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Rising Voice']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'Voluntary']);
+        PostSubType::create([ 'post_type_id' => 4,'name' => 'For People']);
     }
 }
